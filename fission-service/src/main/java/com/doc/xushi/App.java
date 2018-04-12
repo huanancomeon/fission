c725022e96fe6c269bc6e736f1eccdd0a4466b4b
huanan <453794811@qq.com>
2018-04-13 01:03:02 +0800

    shiwu

--- fission-service/src/main/java/com/doc/xushi/App.java
+++ fission-service/src/main/java/com/doc/xushi/App.java
@@ -3,12 +3,14 @@ package com.doc.xushi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by 5000 on 2018/4/1.
  */
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan("com.doc.xushi.mapper")
 public class App {
 
--- fission-service/src/main/java/com/doc/xushi/service/OrderServiceImpl.java
+++ fission-service/src/main/java/com/doc/xushi/service/OrderServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
@@ -33,6 +34,7 @@ public class OrderServiceImpl {
     @Autowired
     GoodsMapper goodsMapper;
 
+    @Transactional(rollbackFor = Exception.class)
     public void addOrder(OrderPo orderPo) throws Exception {
         if (orderPo == null || CollectionUtils.isEmpty(orderPo.getGoodsList())) {
             LOGGER.error("订单参数异常：订单信息：{}", JSON.toJSONString(orderPo));
