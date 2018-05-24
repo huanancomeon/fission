44b2b4b1c904ee90c0928981c3aa931a89a5af0e
lihuanan <lihuanan@le.com>
2018-05-24 23:23:18 +0800

    create

--- fission-service/src/main/java/com/doc/xushi/controller/UserController.java
+++ fission-service/src/main/java/com/doc/xushi/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.doc.xushi.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,10 +19,14 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/user")
 public class UserController extends BaseController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    private static final String WX_LOGIN_URL = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=474469_h&lang=zh_CN&openid=";
+
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping("/wx")
+    @RequestMapping("/login")
     public ResponseEntity<String> weCartLogin() {
         HttpHeaders headers = new HttpHeaders();
         headers.set("auth_token", "asdfgh");
@@ -28,8 +37,20 @@ public class UserController extends BaseController {
         parm.put("parm", "1234");
 
         HttpEntity<JSONObject> entity = new HttpEntity<JSONObject>(parm, headers);
-//        restTemplate.execute("http://localhost:8080/headerApi", HttpMethod.POST, entity, String.class);
+        restTemplate.getForObject("", String.class);
         return null;
     }
+
+    @RequestMapping("info")
+    public ResponseEntity<String> getUserInfo(String openId) {
+
+        String url = WX_LOGIN_URL + openId;
+        String userInfoStr = restTemplate.getForObject(url, String.class);
+        LOGGER.info("getUserInfo:{}",userInfoStr);
+
+        return responseOK(userInfoStr);
+    }
+
+
 
 }
